3234790677bc4c0a41977c61c68d27e6c87ce6db
Michelle Di Guglielmo <mfdigug@gmail.com>
2025-01-04 11:16:29 +1100

    done with arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
-// Write your code here
+const num1 = 2;
+const num2 = 31;
+let multiply = num1 * num2;
+
+
+
+let random = Math.round(Math.random() * 10) + 1;
+
+
+const num3 = 25;
+const num4 = 7;
+let mod = num3 % num4;
+
+
+let max = (1, 2, 7, 20);
+math.max(max);
